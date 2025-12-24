07ebe1fbc889a585d72f913878c5a921242f14c5
Kejax <kejax@kejax.net>
2025-12-24 13:29:35 +0100

    Added OpenGraph to index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+        <meta property="og:title" content="PaketNord Tracker">
+        <meta property="og:description" content="Tracke deine Minecraft Sendungen online">
+
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
